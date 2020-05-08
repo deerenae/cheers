ab9b60304060fe8b3022085a7c301660df2ff63c
github username <github email address>
2020-05-07 21:49:17 -0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import DrinksContainer from './DrinksContainer';
+import * as ReactBootstrap from 'react-bootstrap';
 
 class App extends Component {
 
@@ -39,6 +40,23 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Cheers!</h1>
+        <ReactBootstrap.Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <ReactBootstrap.Navbar.Brand id="nav-title" href="#home">Cheers!</ReactBootstrap.Navbar.Brand>
+  <ReactBootstrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <ReactBootstrap.Navbar.Collapse id="responsive-navbar-nav">
+    <ReactBootstrap.Nav className="mr-auto">
+      <ReactBootstrap.NavDropdown title="Drinks" id="collasible-nav-dropdown">
+        <ReactBootstrap.NavDropdown.Item href="#action/3.1">Martinis</ReactBootstrap.NavDropdown.Item>
+        <ReactBootstrap.NavDropdown.Item href="#action/3.2">Margaritas</ReactBootstrap.NavDropdown.Item>
+        <ReactBootstrap.NavDropdown.Item href="#action/3.3">Sours</ReactBootstrap.NavDropdown.Item>
+        <ReactBootstrap.NavDropdown.Divider />
+        <ReactBootstrap.NavDropdown.Item href="#action/3.4">Random Drink</ReactBootstrap.NavDropdown.Item>
+      </ReactBootstrap.NavDropdown>
+    </ReactBootstrap.Nav>
+    <ReactBootstrap.Nav> 
+    </ReactBootstrap.Nav>
+  </ReactBootstrap.Navbar.Collapse>
+</ReactBootstrap.Navbar>
         <DrinksContainer martinis={this.state.martinis} margaritas={this.state.margaritas} sours={this.state.sours} />
       </div>
     );
